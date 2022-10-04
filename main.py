0f465a344eef6c6aa05969bc6be1fc1ab9bbc91e
Kyuwoong Lee <kwooler@gmail.com>
2022-10-04 17:46:42 +0000

    1

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = [0] * 5
+
+for i in range(5):
+	numbers[i] = int(input())
+	if i == 0 or min > numbers[i]:
+		min = numbers[i]
+	if i == 0 or max < numbers[i]:
+		max = numbers[i]
+total = sum(numbers) - min - max
+print (total)
